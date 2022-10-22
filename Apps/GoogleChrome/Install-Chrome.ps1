bbf3d9536e5077940ff57d66ba1f0d451db7b507
Leroy D'Souza <leroy.dsouza@gmail.com>
2022-10-22 21:24:21 +1100

    commit

--- Apps/GoogleChrome/Install-Chrome.ps1
+++ Apps/GoogleChrome/Install-Chrome.ps1
@@ -1,16 +1,31 @@
+<# 
+.SYNOPSIS
+Install Google Chrome on Azure Virtual 
+.DESCRIPTION
 
-Start-Transcript $(Join-Path $env:TEMP Install-Chrome.log)
+#>
+
+$AppName = 'Google Chrome'
+$EverGreenAppName = 'GoogleChrome'
+
+Start-Transcript $(Join-Path $env:TEMP Install-$($AppName).log)
 Write-Host 'AIB Customization: Google_Chrome'
-$Path = "$env:SystemDrive\Apps\Google\Chrome"
+$Path = "$env:SystemDrive\Apps\$($AppName)"
 
-# Evergreen Needed
+# Evergreen Needed (https://github.com/aaronparker/evergreen)
+If (!(Get-Module -Name Evergreen -ListAvailable -ErrorAction SilentlyContinue))
+    {
+        Install-Module -Name Evergreen -scope AllUsers -Force
+    }
 Import-Module Evergreen
 
 #Download latest installer
-$Chrome = Get-EvergreenApp -Name "GoogleChrome" | Where-Object {  $_.Architecture -eq "x64" -and $_.Channel -eq "stable" } | Select-Object -First 1
-$OutFile = Save-EvergreenApp -InputObject $Chrome -CustomPath $Path -WarningAction "SilentlyContinue"
-$ArgumentList = "/i $($OutFile.FullName) ALLUSERS=1 NOGOOGLEUPDATE=1 /qn"
-$result = Start-Process -FilePath msiexec.exe -ArgumentList  $ArgumentList -NoNewWindow  -wait -passthru
+$App = Get-EvergreenApp -Name $EverGreenAppName | Where-Object {  $_.Architecture -eq "x64" -and $_.Channel -eq "stable" } | Select-Object -First 1
+Write-Host 'AIB Customization: Found version' $App.version
+
+$OutFile = Save-EvergreenApp -InputObject $App -CustomPath "$Path" -WarningAction "SilentlyContinue"
+$ArgumentList = "/i `"$($OutFile.FullName)`" ALLUSERS=1 NOGOOGLEUPDATE=1 /qn"
+$result = Start-Process -FilePath msiexec.exe -ArgumentList  "$ArgumentList" -NoNewWindow  -wait -passthru
 Write-Host 'AIB Customization Error Message: ' $error[0]
 
 
@@ -24,4 +39,4 @@ Write-Host 'AIB Customization Exit code: ' $LASTEXITCODE
 Write-Host 'AIB Customization Error Message: ' $error[0]
 
 #Cleanup installers
-Remove-Item $Path -Force
+Remove-Item "$Path" -Force -Recurse -Verbose
