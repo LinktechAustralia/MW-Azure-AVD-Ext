c3b9c3f493fed4fc83ba6d5a7dedaf13fb4bf0dd
Leroy D'Souza <Leroy.dsouza@linktechaustralia.com.au>
2022-12-03 02:57:31 +1100

    commit

---
+++ Apps/AZCopy/Install-AzCopy.ps1
@@ -0,0 +1,16 @@
+#Get AZCOPY
+$azcopytemp = join-path -path $env:TEMP -childpath  "azcopy_" 
+$azcopyzip = join-path -path $env:TEMP -childpath  "azcopy.zip"
+$URL = "https://aka.ms/downloadazcopy-v10-windows"
+
+
+if ( -not (test-path -path $azcopyzip  )) {
+	Write-Host "Downloading Azcopy " $azcopyzip -ForegroundColor Green
+	Invoke-WebRequest -Uri $URL -OutFile $azcopyzip -erroraction 'silentlycontinue'
+	Write-Host "Azcopy downloaded in "  $ImageUSB.azcopypath -ForegroundColor Green
+}
+
+Expand-Archive $azcopyzip $azcopytemp  -Force -Verbose
+$azcopyexe = Get-ChildItem $azcopytemp -Include *.exe -Recurse
+saps "C:\Windows\System32\Robocopy.exe" -args "$($azcopyexe.DirectoryName) $env:windir\system32 $($azcopyexe.Name)" -NoNewWindow
+
