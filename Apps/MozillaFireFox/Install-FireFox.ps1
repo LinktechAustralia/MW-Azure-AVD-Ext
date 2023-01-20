fea09a5056521f42278512a18e54e6913f382f89
Leroy D'Souza <Leroy.dsouza@linktechaustralia.com.au>
2023-01-21 01:42:18 +1100

    commit

--- Apps/MozillaFireFox/Install-FireFox.ps1
+++ Apps/MozillaFireFox/Install-FireFox.ps1
@@ -12,27 +12,12 @@ Start-Transcript $(Join-Path $env:TEMP Install-$($AppName).log)
 Write-Host "AIB Customization: $($AppName)"
 $Path = "$env:SystemDrive\Apps\$($AppName)"
 
-# Evergreen Needed (https://github.com/aaronparker/evergreen)
-If (!(Get-Module -Name Evergreen -ListAvailable -ErrorAction SilentlyContinue))
-    {
-        Install-Module -Name Evergreen -scope AllUsers -Force
-    }
-Import-Module Evergreen
+md "$Path"
 
-#Download latest installer
-$App = Get-EvergreenApp -Name $EverGreenAppName | Where-Object {  $_.Architecture -eq "x64" -and $_.Channel -eq "LATEST_FIREFOX_VERSION" -and $_.Type -eq "msi"} | Select-Object -First 1
-Write-Host 'AIB Customization: Found version' $App.version
-
-$OutFile = Save-EvergreenApp -InputObject $App -CustomPath "$Path" -WarningAction "SilentlyContinue"
-$ArgumentList = "/i `"$($OutFile.FullName)`" /qn DESKTOP_SHORTCUT=false INSTALL_MAINTENANCE_SERVICE=false TASKBAR_SHORTCUT=false REGISTER_DEFAULT_AGENT=false"
-$result = Start-Process -FilePath msiexec.exe -ArgumentList  "$ArgumentList" -NoNewWindow  -wait -passthru
-Write-Host 'AIB Customization Error Message: ' $error[0]
-
-
-
-
-Write-Host 'AIB Customization Exit code: ' $LASTEXITCODE
-Write-Host 'AIB Customization Error Message: ' $error[0]
-
+winget.exe install --id mozilla.firefox --accept-package-agreements --scope machine --accept-source-agreements --silent
+Write-Host "AIB Customization $($AppName) Exit code: " $LASTEXITCODE
+if ($error[0]) {
+	Write-Host "AIB Customization Error Message: " $error[0]
+}
 #Cleanup installers
 Remove-Item "$Path" -Force -Recurse -Verbose
