8b05f066f4a42e96449c938f0e7ecb8524f7fb78
Leroy D'Souza <Leroy.dsouza@linktechaustralia.com.au>
2023-01-21 22:08:39 +1100

    commit

--- Apps/MozillaFireFox/Install-FireFox.ps1
+++ Apps/MozillaFireFox/Install-FireFox.ps1
@@ -19,8 +19,10 @@ $Path = "$env:SystemDrive\Apps\$($AppName)"
 md "$Path" -Force
 # Unattended Install of Firefox
 
-$SourceURL = "https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=win64&lang=en-US"
+#$SourceURL = "https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=win64&lang=en-US"
+$SourceURL = "https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US"
 $Installer = Join-path $Path "firefox.exe"; 
-Invoke-WebRequest $SourceURL -OutFile $Installer -UseBasicParsing -Verbose
+#Invoke-WebRequest $SourceURL -OutFile $Installer -UseBasicParsing -Verbose
+Start-BitsTransfer $SourceURL -Destination $Installer
 Start-Process -FilePath $Installer -Args "/s /TaskbarShortcut=false /DesktopShortcut=false /MaintenanceService=false /RegisterDefaultAgent=false" -NoNewWindow -Wait -PassThru -Verbose
 Remove-Item $Path -Force -Verbose
