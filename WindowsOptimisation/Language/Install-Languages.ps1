a3c10b2937d46c17bfe4c8aa4ff5e26852b5c1db
Leroy D'Souza <Leroy.dsouza@linktechaustralia.com.au>
2023-01-23 17:46:02 +1100

    commit

--- WindowsOptimisation/Language/Install-Languages.ps1
+++ WindowsOptimisation/Language/Install-Languages.ps1
@@ -6,20 +6,37 @@ View a list of all language packs at https://learn.microsoft.com/en-us/windows-h
 $R
 #>
 
+Start-Transcript "C:\Windows\Logs\Install-LangugeManagement.log"
 
-Import-Module Languagepackmanagement
+"Language Installer: Importing Module"
+Import-Module Languagepackmanagement -Verbose
 
-$RequiredLanguages = @('en-AU')
+$RequiredLanguages = @('en-AU','pt-BR')
 $DefaultLanguage = 'en-AU'
 
 foreach ($ReqLang in $RequiredLanguages)
 	{
+		"Language Installer: Checking for $($ReqLang)"
 		if (!(Get-Language -Language $ReqLang)) {
+			"Language Installer: $($ReqLang) is not installed. Installing.."
 			Install-Language -Language $ReqLang -CopyToSettings -Verbose
 
 		} else {
-			"$($ReqLang) is installed"
+			"Language Installer: $($ReqLang) is installed"
 		}
 	}
 
 Set-SystemPreferredUILanguage -Language $DefaultLanguage -PassThru -Verbose
+Set-WinSystemLocale -SystemLocale $DefaultLanguage
+
+#Cleaning Up
+Write-Host "Language Installer: Cleaning up Languages"
+$InstalledLanguages = get-language 
+foreach ($InstalledLang in $InstalledLanguages){
+	"Checking $($InstalledLang.LanguageId)"
+	if ($($InstalledLang.LanguageId) -notin $RequiredLanguages) {
+		"Cleaning $($InstalledLang.LanguageId)" 
+		Uninstall-Language $($InstalledLang.LanguageId) -PassThru
+	}
+}
+
