1c3933362ccaa10b12781cf07e796eb1cfb401d8
Leroy D'Souza <Leroy.dsouza@linktechaustralia.com.au>
2023-02-09 23:27:18 +1100

    commit

--- Apps/AdobeAcrobatPro/Install-AdobeAcrobatPro.ps1
+++ Apps/AdobeAcrobatPro/Install-AdobeAcrobatPro.ps1
@@ -1,9 +1,19 @@
 <# 
 .SYNOPSIS
-Install Adobe Acrobat Pro on an image created with  Azure Image Builder 
+Install Adobe Acrobat Pro on an image created with Azure Image Builder 
 .DESCRIPTION
-The script invokes a download of the latest Office Deplyment Toolkit.
-Extracts the toolkit
+	- The script invokes a download of the latest installer by parsing the Adobe website.
+	- Extracts the zip
+	- Performs the install as per standard deplyment
+
+	The argument list can be customised as required ref $ArgumentList
+
+	Further policies / customisations can be done by updating the customisation region 
+	Check https://www.adobe.com/devnet-docs/acrobatetk/tools/PrefRef/Windows/Originals.html for more policies
+.NOTES
+	Created by Linktech Australia
+	Author: Leroy D'Souza
+.LINK
 
 #>
 Function LogDate {"$(Get-date -fo s)" + "`t"}
@@ -18,25 +28,28 @@ if (!(Test-Path $Path -ErrorAction SilentlyContinue)) #Create the folder if it d
 
 #Download the Installer
 
-$URI = 'https://helpx.adobe.com/au/acrobat/kb/acrobat-dc-downloads.html'
+	$URI = 'https://helpx.adobe.com/au/acrobat/kb/acrobat-dc-downloads.html'
 
-$URIRef = Invoke-WebRequest -Uri $URI -UseBasicParsing 
-$DownloadLink = $URIRef.Links | ? { $_.outerHTML -like '*trials*'-and $_.outerHTML -like '*x64*'} | select -First 1
-$DownloadLink = $DownloadLink.href
-$DownloadFileName = $([System.IO.Path]::GetFileName($DownloadLink.ToString()))
-$Global:Destination = Join-path "$Path" $DownloadFileName
-Start-BitsTransfer -Source $DownloadLink -Destination $Destination
+	$URIRef = Invoke-WebRequest -Uri $URI -UseBasicParsing 
+	$DownloadLink = $URIRef.Links | ? { $_.outerHTML -like '*trials*'-and $_.outerHTML -like '*x64*'} | select -First 1
+	$DownloadLink = $DownloadLink.href
+	$DownloadFileName = $([System.IO.Path]::GetFileName($DownloadLink.ToString()))
+	$Global:Destination = Join-path "$Path" $DownloadFileName
+	Start-BitsTransfer -Source $DownloadLink -Destination $Destination
 
-Write-host "AIB Customization: Extracting $DownloadFileName"
-Expand-Archive $Destination "$Path" -Force 
+# Extract the installer
+	Write-host "AIB Customization: Extracting $DownloadFileName"
+	Expand-Archive $Destination "$Path" -Force 
 
-$InstallerPath = Join-path $path "Adobe Acrobat"
-$InstallerFullFilePath = Join-Path  $InstallerPath setup.EXE
-$ArgumentList = '/sAll /rs /msi  EULA_ACCEPT=YES LANG_LIST=en_US UPDATE_MODE=0 DISABLE_ARM_SERVICE_INSTALL=1 ADD_THUMBNAILPREVIEW=YES'
-Start-Process $InstallerFullFilePath -ArgumentList $ArgumentList -NoNewWindow -PassThru -Wait
+# Begin Install
+	$InstallerPath = Join-path $path "Adobe Acrobat"
+	$InstallerFullFilePath = Join-Path  $InstallerPath setup.EXE
+	$ArgumentList = '/sAll /rs /msi  EULA_ACCEPT=YES LANG_LIST=en_US UPDATE_MODE=0 DISABLE_ARM_SERVICE_INSTALL=1 ADD_THUMBNAILPREVIEW=YES'
+	Start-Process $InstallerFullFilePath -ArgumentList $ArgumentList -NoNewWindow -PassThru -Wait
 
-#Customisation
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown' -Value 1 -Name bIsSCReducedModeEnforcedEx -PropertyType DWORD -Force
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cServices' -Value 1 -Name bUpdater -PropertyType DWORD -Force
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cServices' -Value 0 -Name bToggleAdobeSign -PropertyType DWORD -Force
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cIPM' -Value 0 -Name bDontShowMsgWhenViewingDoc -PropertyType DWORD -Force
+#region Customisation
+	New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown' -Value 1 -Name bIsSCReducedModeEnforcedEx -PropertyType DWORD -Force
+	New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cServices' -Value 1 -Name bUpdater -PropertyType DWORD -Force
+	New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cServices' -Value 0 -Name bToggleAdobeSign -PropertyType DWORD -Force
+	New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cIPM' -Value 0 -Name bDontShowMsgWhenViewingDoc -PropertyType DWORD -Force
+	#endregion Customisation 
